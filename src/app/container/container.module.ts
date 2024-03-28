3c1013757b0254749f91a91e58d0b8c4144af79f
Brayan Ramos <brayanrcaicedo@gmail.com>
2024-03-27 20:51:44 -0500

    create notification module

--- src/app/container/container.module.ts
+++ src/app/container/container.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { ContainerComponent } from './container.component';
 import { MenuComponent } from './menu/menu.component';
 import { HeaderComponent } from './header/header.component';
+import { NotificationModule } from '../notification/notification.module';
 
 @NgModule({
   declarations: [ContainerComponent, MenuComponent, HeaderComponent],
+  imports: [NotificationModule],
   exports: [ContainerComponent]
 })
 export class ContainerModule { }
